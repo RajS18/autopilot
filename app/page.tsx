85d702f0048a5a02283f6d898d8370fd770b053f
Raj Shukla <rajdragatron918@gmail.com>
2026-07-19 23:57:07 -0400

    Small Fix on Page layout before adding org

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,5 @@
-"use client"
-
-import { toast } from "sonner"
-
-import { Button } from "@/components/ui/button"
+import { UserButton } from "@clerk/nextjs"
 
 export default function Page() {
-  return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          
-          <Button className="mt-2" onClick={() => toast("Button clicked")}>Button</Button>
-        </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
-        </div>
-      </div>
-    </div>
-  )
+  return <UserButton />
 }
